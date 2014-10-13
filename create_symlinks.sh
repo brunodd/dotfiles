59e13b7b28cb2e3f3f84673ccda75862e4dfbdbd
brunodd <dedekenbruno@gmail.com>
2014-10-13 09:24:42 +0200

    Added .vim/ dir to dotfiles

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -6,4 +6,4 @@ ln -s ~/dotfiles/.c_header.txt ~/.c_header.txt
 ln -s ~/dotfiles/.cpp_header.txt ~/.cpp_header.txt
 ln -s ~/dotfiles/.cc_header.txt ~/.cc_header.txt
 ln -s ~/dotfiles/ftplugin ~/.vim/ftplugin
-ln -s ~/dotfiles/calmar256-dark.vim ~/.vim/colors/calmar256-dark.vim
+ln -s ~/dotfiles/.vim/ ~/.vim
