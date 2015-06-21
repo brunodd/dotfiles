d3c4073aa4f4334bb5d50351469fbe7cfb71f4bb
brunodd <dedekenbruno@gmail.com>
2015-06-21 16:44:59 +0200

    Added msg in create_symlinks

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-if [[ ! -e ~/.vimrc ]]
+if [[ -e ~/.vimrc ]]
 then
+	echo "~/.vimrc exists. It was ignored."
+else
 	ln -s ~/dotfiles/.vimrc ~/.vimrc
 fi
 
-if [[ ! -e ~/.vim/ftplugin/ ]]
+if [[ -e ~/.vim/ftplugin/ ]]
 then
+	echo "~/.vim/ftplugin/ exists. It was ignored."
+else
 	ln -s ~/dotfiles/ftplugin ~/.vim/ftplugin
 fi
 
-if [[ ! -e ~/.vim/colors/ ]]
+if [[ -e ~/.vim/colors/ ]]
 then
+	echo "~/.vim/colors exists. It was ignored."
+else
 	ln -s ~/dotfiles/colors ~/.vim/colors
 fi
